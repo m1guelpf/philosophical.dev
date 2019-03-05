7a72b8ea85dc6af1f1f00a15190a18719933743b
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-03-05 21:16:23 +0100

    Open new links in new tab

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -40,6 +40,16 @@ export default {
     PostMeta,
     PostTags
   },
+  mounted() {
+    var links = document.links;
+
+    for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+      if (links[i].hostname != window.location.hostname) {
+        links[i].target = '_blank';
+        links[i].rel = 'noopener noreferer'
+      }
+    }
+  },
   metaInfo () {
     return {
       title: this.$page.post.title,
