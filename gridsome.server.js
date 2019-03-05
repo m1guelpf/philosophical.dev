2875da6112f5d8d9760a8b3a15d673b8b7851d1c
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-03-05 21:22:58 +0100

    debug

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,7 +4,6 @@
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-exit();
 const config = require('./gridsome.config');
 
 const GhostContentAPI = require('@tryghost/content-api')
@@ -72,6 +71,7 @@ module.exports = function (api) {
         }).includes('#philosophy')
       })
     }).then(articles => {
+      console.log(articles)
       articles.forEach(post => {
         posts.addNode({
           title: post.title,
